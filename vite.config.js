3bce6e43c452247202e6e83bac5de77c0747a71b
susmisc14 <su.smisc14@gmail.com>
2025-07-31 01:23:19 +0900

    React 의존성 추가: React 19, ESLint, TypeScript 설정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
   },
-})
+});
